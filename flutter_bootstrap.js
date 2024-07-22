36c58497f0b64aa8615b64cff46a2f8043f11802
= <=>
2024-07-22 15:16:53 -0600

    sfdds

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2410130492"
+    serviceWorkerVersion: "2386932577"
   }
 });
